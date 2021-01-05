4577bda7198656c0edf99b84284b004f4ad5fef1
muhammadirfan5 <muhammadirfan5891@gmail.com>
2021-01-05 21:03:31 +0500

    Users,Roles,Complains,Profile and Suggestions Update By Muhammad Irfan.

---
+++ db-backups/1-5-2021-V2/hztechemployement.sql
@@ -0,0 +1,382 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3308
+-- Generation Time: Jan 05, 2021 at 04:51 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hztechemployement`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applications`
+--
+
+CREATE TABLE `applications` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `application_user`
+--
+
+CREATE TABLE `application_user` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complains`
+--
+
+CREATE TABLE `complains` (
+  `id` int(11) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `complains`
+--
+
+INSERT INTO `complains` (`id`, `title`, `description`, `status`, `user_id`, `created_at`, `updated_at`) VALUES
+(3, 'Iure ea fugiat aut a', 'Est aute cupidatat r', 'Reject', 19, '2021-01-04 09:23:49', '2021-01-04 09:24:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complain_user`
+--
+
+CREATE TABLE `complain_user` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `privileges`
+--
+
+CREATE TABLE `privileges` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `privilege_user`
+--
+
+CREATE TABLE `privilege_user` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE `requests` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request_user`
+--
+
+CREATE TABLE `request_user` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `title`, `description`, `status`, `created_at`, `updated_at`) VALUES
+(12, 'lead', 'Non aut modi quos et', 'Block', '2021-01-01 13:25:46', '2021-01-01 13:25:46'),
+(13, 'head', 'Sunt sint anim esse', 'Block', '2021-01-01 13:29:50', '2021-01-01 13:29:50'),
+(14, 'hr', 'Numquam libero moles', 'Active', '2021-01-01 13:30:33', '2021-01-01 13:30:33'),
+(15, 'employee', 'Employee', 'Active', '2021-01-05 11:25:01', '2021-01-05 11:25:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_user`
+--
+
+CREATE TABLE `role_user` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suggestions`
+--
+
+CREATE TABLE `suggestions` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `suggestions`
+--
+
+INSERT INTO `suggestions` (`id`, `title`, `description`, `status`, `user_id`, `created_at`, `updated_at`) VALUES
+(7, 'Non nesciunt quia o', 'Voluptatem Officiis', 'Pending', 18, '2021-01-04 08:40:13', '2021-01-04 08:40:13'),
+(8, 'Et ipsum eos rerum', 'Dolores quia dolor v', 'Approved', 18, '2021-01-04 08:40:19', '2021-01-04 08:40:52'),
+(9, 'Aliquip fugiat dolor', 'Quidem explicabo Et', 'Reject', 18, '2021-01-04 08:40:31', '2021-01-04 08:40:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suggestion_user`
+--
+
+CREATE TABLE `suggestion_user` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `suggestion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fullname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `designation` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone_number` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `profile_pic` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `string_password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `fullname`, `email`, `designation`, `address`, `phone_number`, `role_id`, `status`, `profile_pic`, `email_verified_at`, `password`, `string_password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(16, 'Illana', 'Lucy', 'Bert', 'admin@gmail.com', 'Software Engineer', 'Voluptas voluptate q', '+1 (194) 385-3698', 13, 'Active', 'upload/user/16/92dr6QZ7ghdwvgxD5Q2GXKsBCHOOfK56sazQit5R.jpg', NULL, '$2y$10$TY9ZMZNGshuHOFSCMYatAepLTStmrfKwW0Fr/iJ6/bppBF0RtzWjm', 'Pa$$w0rd!', NULL, '2021-01-01 08:16:16', '2021-01-05 10:27:18'),
+(18, 'Shelly', 'Aline', 'Shelly Aline', 'shellyalnie@gmail.com', 'Laravel Developer', '16/A South bay street, huston texas, USA', '03462338888', 14, 'Block', 'upload/user/18/oxKIEd2GEUIWUPuIdGDJZCm7jl3Qye1DqAV6QoKB.jpg', NULL, '$2y$10$r.dYafifxehurzyIRR5vGOLNFStU5xoh2Dv0XSIKchEbAbsymiIy6', 'Pa$$w0rd!', NULL, '2021-01-01 08:18:33', '2021-01-05 02:27:42'),
+(19, NULL, NULL, 'Ciara Garrison', 'xexiv@mailinator.com', NULL, NULL, NULL, 12, 'Block', NULL, NULL, '$2y$10$xymjqlhLKm1E9XFdE4Rc/.DtGgv2uk7rOaA5DGIgZLb2ra3bseDZW', 'Pa$$w0rd!', NULL, '2021-01-01 09:25:52', '2021-01-01 09:25:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_metas`
+--
+
+CREATE TABLE `user_metas` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `meta_key` varchar(500) DEFAULT NULL,
+  `meta_value` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_metas`
+--
+
+INSERT INTO `user_metas` (`id`, `user_id`, `meta_key`, `meta_value`) VALUES
+(1, 16, 'city', 'Josephine'),
+(2, 16, 'state', 'Hunter'),
+(3, 16, 'martial_status', 'Married'),
+(4, 16, 'cnic', 'Not Found'),
+(5, 16, 'dob', '28/10/1995'),
+(6, 16, 'zipcode', NULL),
+(7, 16, 'alt_phone', '+1 (194) 385-3698'),
+(8, 16, 'em_first_name', 'Uriah'),
+(9, 16, 'em_last_name', 'Oconnor'),
+(10, 16, 'em_full_name', 'Burton Parks'),
+(11, 16, 'em_relationship', 'Hic nesciunt ex eve'),
+(12, 16, 'em_city', 'Consequatur Debitis'),
+(13, 16, 'em_state', 'Possimus nobis aliq'),
+(14, 16, 'em_address', 'Dolore duis debitis'),
+(15, 16, 'em_phone_number', '+1 (702) 127-2754'),
+(16, 16, 'em_al_phone', '+1 (581) 844-8399'),
+(17, 16, 'em_zipcode', '80272');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `complains`
+--
+ALTER TABLE `complains`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `suggestions`
+--
+ALTER TABLE `suggestions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- Indexes for table `user_metas`
+--
+ALTER TABLE `user_metas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `complains`
+--
+ALTER TABLE `complains`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `suggestions`
+--
+ALTER TABLE `suggestions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `user_metas`
+--
+ALTER TABLE `user_metas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
